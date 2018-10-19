f7073bfd82172834dadc9fbed8df0f7d1aadacb6
Eleanorigby <jenn08@msn.com>
2018-10-18 18:47:11 -0600

    add namespace in controller and remove CRUD from application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,28 +1,3 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :null_session
-
-  def index
-    render json: Task.all
-  end
-
-  def create
-    Task = Task.create(Task_params)
-    render json: Task
-  end
-
-  def destroy
-    Task.destroy(params[:id])
-  end
-
-  def update
-    Task = Task.find(params[:id])
-    Task.update_attributes(Task_params)
-    render json: Task
-  end
-
-  private
-
-  def Task_params
-    params.require(:Task).permit(:id, :name, :description)
-  end
 end
